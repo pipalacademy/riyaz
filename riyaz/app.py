7c91c7decbc53fb1122663a7f9b207d7ae8edcf3
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2022-08-15 20:38:05 +0530

    #2: Add course-header section for course page

--- riyaz/app.py
+++ riyaz/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
 
+from . import courses
+
 
 app = Flask("riyaz")
+app.register_blueprint(courses.bp, url_prefix="/courses")
 
 
 @app.route("/")
@@ -16,8 +19,9 @@ def index():
                 {
                     "id": "ab8bcec7-b384-46b6-b496-0d6df4615a3e",
                     "name": "Alice",
+                    "photo": "https://thispersondoesnotexist.com/image",
                 }
-            ]
+            ],
         },
         {
             "id": "d7965fec-a530-4cb3-b739-3ce074145bce",
@@ -28,8 +32,9 @@ def index():
                 {
                     "id": "b576389e-74d7-46fe-8019-e0262a1ea35c",
                     "name": "Bob",
+                    "photo": "https://thispersondoesnotexist.com/image",
                 }
-            ]
+            ],
         },
         {
             "id": "e86948f5-5593-430b-966d-3cf611870db4",
@@ -40,9 +45,10 @@ def index():
                 {
                     "id": "84517dac-33af-4cb7-8dff-7acc1fadb360",
                     "name": "Chuck",
+                    "photo": "https://thispersondoesnotexist.com/image",
                 }
-            ]
-        }
+            ],
+        },
     ]
 
     return render_template("index.html", courses=placeholder_courses)
---
+++ riyaz/courses.py
@@ -0,0 +1,31 @@
+from flask import Blueprint, render_template
+
+
+bp = Blueprint("courses", __name__)
+
+
+@bp.route("/<name>")
+def view_course(name: str):
+    course = get_course(name=name)
+    return render_template("course.html", course=course)
+
+
+def get_course(name):
+    return {
+        "id": "a1afe86d-8a99-489e-b3f8-7d2be764a857",
+        "name": "alpha-course",
+        "title": "Alpha Course",
+        "short_description": "A course on Alpha",
+        "authors": [
+            {
+                "id": "ab8bcec7-b384-46b6-b496-0d6df4615a3e",
+                "name": "Alice",
+                "photo": "https://thispersondoesnotexist.com/image",
+            },
+            {
+                "id": "84517dac-33af-4cb7-8dff-7acc1fadb360",
+                "name": "Chuck",
+                "photo": "https://thispersondoesnotexist.com/image",
+            },
+        ],
+    }
