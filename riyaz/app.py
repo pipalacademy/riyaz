aefcc089c16d46cb1bfd61b134dfe3bcc5ab0cba
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2022-08-15 19:16:28 +0530

    issue #2: Add home page with featured courses

--- riyaz/app.py
+++ riyaz/app.py
@@ -11,6 +11,7 @@ def index():
             "id": "a1afe86d-8a99-489e-b3f8-7d2be764a857",
             "name": "alpha-course",
             "title": "Alpha Course",
+            "short_description": "A course on Alpha",
             "authors": [
                 {
                     "id": "ab8bcec7-b384-46b6-b496-0d6df4615a3e",
@@ -22,6 +23,7 @@ def index():
             "id": "d7965fec-a530-4cb3-b739-3ce074145bce",
             "name": "bravo-course",
             "title": "Bravo Course",
+            "short_description": "A course on Bravo",
             "authors": [
                 {
                     "id": "b576389e-74d7-46fe-8019-e0262a1ea35c",
@@ -33,6 +35,7 @@ def index():
             "id": "e86948f5-5593-430b-966d-3cf611870db4",
             "name": "charlie-course",
             "title": "Charlie Course",
+            "short_description": "A course on Charlie",
             "authors": [
                 {
                     "id": "84517dac-33af-4cb7-8dff-7acc1fadb360",
