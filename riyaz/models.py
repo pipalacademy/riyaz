6d63dd0789685e83e615e99455672edfdc09917a
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2022-08-16 19:28:59 +0530

    Update sample data and templates with schema fixes

--- riyaz/models.py
+++ riyaz/models.py
@@ -4,4 +4,29 @@ from . import db
 class Course(db.Document):
     pass
 
+class Module(db.Document):
+    pass
+
+class Lesson(db.Document):
+    @property
+    def module(self):
+        parts = self.key.split("/", 1)
+        if len(parts) == 2:
+            module_key = parts[0]
+            return db.get("module", module_key)
+        else:
+            return None
+
+    @property
+    def next(self):
+        collection = db.query("lesson", index=self.index+1)
+        return collection and collection[0] or None
+
+    @property
+    def previous(self):
+        collection = db.query("lesson", index=self.index-1)
+        return collection and collection[0] or None
+
 db.register_model("course", Course)
+db.register_model("module", Module)
+db.register_model("lesson", Lesson)
