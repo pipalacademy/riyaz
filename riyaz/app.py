00c611540f5333e400f4d7d2a48cdc52d198046f
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2022-08-16 19:43:11 +0530

    merge courses.py into app.py

--- riyaz/app.py
+++ riyaz/app.py
@@ -1,13 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, abort, render_template
 
+from functools import wraps
 import markdown
 
-from . import courses
 from .models import db
 
 
 app = Flask("riyaz")
-app.register_blueprint(courses.bp, url_prefix="/courses")
+
+
+@app.template_filter("markdown")
+def md_to_html(md: str):
+    return markdown.markdown(md, extensions=["fenced_code"])
 
 
 @app.route("/")
@@ -16,6 +20,42 @@ def index():
     return render_template("index.html", courses=courses)
 
 
-@app.template_filter("markdown")
-def md_to_html(md: str):
-    return markdown.markdown(md, extensions=["fenced_code"])
+@app.route("/courses/<name>")
+def view_course(name: str):
+    course = get_course(name=name)
+    return render_template("course.html", course=course)
+
+
+@app.route("/courses/<course_name>/<module_name>/<lesson_name>")
+def view_lesson(course_name: str, module_name: str, lesson_name: str):
+    course = get_course(name=course_name)
+    lesson = get_lesson(
+        course_id=course.id,
+        module_name=module_name,
+        lesson_name=lesson_name,
+    )
+
+    return render_template("lesson.html", course=course, lesson=lesson)
+
+
+def abort_if_none(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        retval = fn(*args, **kwargs)
+        if retval is None:
+            abort(404)
+
+        return retval
+
+    return wrapper
+
+
+@abort_if_none
+def get_course(name):
+    course = db.get("course", name)
+    return course
+
+
+@abort_if_none
+def get_lesson(course_id: str, module_name: str, lesson_name: str):
+    return db.get("lesson", f"{module_name}/{lesson_name}")
