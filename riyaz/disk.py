d7bc893aba7d296a317f8c0f6b25e66009be6928
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2022-08-20 13:08:48 +0530

    bugfix: pydantic validation when working directory is not same as course directory

--- riyaz/disk.py
+++ riyaz/disk.py
@@ -16,9 +16,17 @@ from .models import Author, Chapter, Course, Lesson
 
 
 class DiskChapter(BaseModel):
+    base_dir: DirectoryPath = Path(".")
     name: str
     title: str
     lessons: List[FilePath]
+
+    @validator("lessons", each_item=True, pre=True)
+    def prepend_base_dir_to_lessons(cls, v, values):
+        if isinstance(v, str) or isinstance(v, Path):
+            return values["base_dir"] / v
+
+        return v
 
     def parse(self) -> Chapter:
         lessons = [get_lesson_from_path(path) for path in self.lessons]
@@ -26,6 +34,7 @@ class DiskChapter(BaseModel):
 
 
 class DiskCourse(BaseModel):
+    base_dir: DirectoryPath = Path(".")
     name: str
     title: str
     short_description: Optional[str]
@@ -34,15 +43,35 @@ class DiskCourse(BaseModel):
     outline: List[DiskChapter]
 
     @validator("authors", each_item=True)
-    def check_author_file_exists(cls, v):
-        author_file = get_author_file_path(v)
+    def check_author_file_exists(cls, v, values):
+        author_file = get_author_file_path(values["base_dir"], v)
         if not author_file.exists():
             raise ValueError(f"author file {author_file} does not exist")
 
         return v
 
+    @validator("outline", each_item=True, pre=True)
+    def add_base_dir_to_outline(cls, v, values):
+        if isinstance(v, dict):
+            v["base_dir"] = v.get("base_dir", values["base_dir"])
+
+        return v
+
+    def get_author_from_key(self, key):
+        path = get_author_file_path(self.base_dir, key)
+        fm = frontmatter.load(path)
+
+        return Author(
+            key=key,
+            name=fm.get("name"),
+            photo=fm.get("photo"),
+            about=fm.content,
+        )
+
     def parse(self) -> Course:
-        authors = [get_author_from_key(key) for key in self.authors]
+        authors = [
+            self.get_author_from_key(key) for key in self.authors
+        ]
         outline = [disk_chapter.parse() for disk_chapter in self.outline]
         return Course(
             name=self.name,
@@ -65,7 +94,10 @@ def read_config(path: FilePath) -> DiskCourse:
     with open(path) as f:
         config_dict = yaml.safe_load(f)
 
-    return DiskCourse.parse_obj(config_dict)
+    config_dict["base_dir"] = config_dict.get("base_dir", path.parent)
+    course = DiskCourse.parse_obj(config_dict)
+
+    return course
 
 
 @validate_arguments
@@ -78,21 +110,8 @@ def get_lesson_from_path(path: FilePath) -> Lesson:
     return Lesson(name=name, title=title, content=content)
 
 
-@validate_arguments
-def get_author_from_key(key: str) -> Author:
-    path = get_author_file_path(key)
-    fm = frontmatter.load(path)
-
-    return Author(
-        key=key,
-        name=fm.get("name"),
-        photo=fm.get("photo"),
-        about=fm.content,
-    )
-
-
-def get_author_file_path(name: str) -> Path:
-    return Path(f"authors/{name}.md")
+def get_author_file_path(base_dir: DirectoryPath, key: str):
+    return base_dir / "authors" / f"{key}.md"
 
 
 def get_first_heading(content: str) -> Optional[str]:
--- tests/test_disk.py
+++ tests/test_disk.py
@@ -7,12 +7,12 @@ from riyaz import disk
 from .conftest import get_config_path
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture
 def change_workdir(monkeypatch, data_dir):
     monkeypatch.chdir(data_dir)
 
 
-def test_read_config():
+def test_read_config(change_workdir):
     path = get_config_path("ok")
 
     print(os.getcwd())
@@ -31,15 +31,20 @@ def test_read_config():
     assert isinstance(config.outline[0], disk.DiskChapter)
 
 
-def test_read_config_when_authors_type_is_incorrect():
+def test_read_config_when_authors_type_is_incorrect(change_workdir):
     path = get_config_path("incorrect_authors")
 
     with pytest.raises(ValidationError):
         disk.read_config(path)
 
 
-def test_read_config_when_chapters_type_is_incorrect():
+def test_read_config_when_chapters_type_is_incorrect(change_workdir):
     path = get_config_path("incorrect_chapters")
 
     with pytest.raises(ValidationError):
         disk.read_config(path)
+
+
+def test_course_from_directory(data_dir):
+    course = disk.get_course_from_directory(data_dir)
+    assert course.name == "sample-course"
