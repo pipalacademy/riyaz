8ebd8151e85f9d51d3b555c377be850ed373da58
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2022-08-29 17:46:56 +0530

    Add riyaz plugin

---
+++ riyaz/plugins/feather.py
@@ -0,0 +1,41 @@
+from riyaz.app import include_stylesheet, include_javascript
+from riyaz import config
+
+
+def get_modes():
+    modes = getattr(config, "modes", ["python"])
+    return modes
+
+
+def get_stylesheets():
+    return [
+        "/static/feather/codemirror/lib/codemirror.css",
+        "/static/feather/feather.css",
+    ]
+
+
+def get_javascripts():
+    mode_js = "/static/feather/codemirror/mode/{mode}/{mode}.js"
+    return [
+        "/static/feather/jquery-3.6.0.min.js",
+        "/static/feather/codemirror/lib/codemirror.js",
+        "/static/feather/codemirror/addon/mode/simple.js",
+        "/static/feather/codemirror/keymap/sublime.js",
+        *[mode_js.format(mode=mode) for mode in get_modes()],
+        "/static/feather/feather.js",
+        "/static/feather/feather_config.js",
+    ]
+
+
+def inject_assets():
+    stylesheets = get_stylesheets()
+    javascripts = get_javascripts()
+
+    for ss in stylesheets:
+        include_stylesheet(ss)
+
+    for js in javascripts:
+        include_javascript(js)
+
+
+inject_assets()
